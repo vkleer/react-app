e8132add1e982e0600a96cb3f696b3aa3bf3a16a
Vilayat Kleer <vkleer@hotmail.com>
2022-10-03 23:16:17 +0000

    Add state to StatefulGreeting class

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -10,11 +10,19 @@ class StatefulGreeting extends React.Component {
         };
     }
 
+    handleClick() {
+        this.setState({
+            introduction: "Goodbye!",
+            buttonText: "Enter",
+        });
+        console.log(this.state.introduction);
+    }
+
     render() {
         return (
             <div>
                 <h1>{this.state.introduction}, {this.props.name}! {this.props.greeting}</h1>
-                <button>{this.state.buttonText}</button>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
             </div>
         )
     }
