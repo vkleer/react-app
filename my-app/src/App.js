e89f3d2a972c8d7bb0112ad583cdfa30f26fed91
Vilayat Kleer <vkleer@hotmail.com>
2022-10-03 23:33:05 +0000

    Create stateful component with callback function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 import StatefulGreeting from './components/StatefulGreeting';
+import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreetingWithProps greeting="nice to meet you!" name="John" age="30" />
-      <StatefulGreeting greeting="I'm a stateful class component!" name="John" />
+      {/* <FunctionalGreetingWithProps greeting="nice to meet you!" name="John" age="30" />
+      <StatefulGreeting greeting="I'm a stateful class component!" name="John" /> */}
+      <StatefulGreetingWithCallback />
     </div>
   );
 }
---
+++ my-app/src/components/StatefulGreetingWithCallback.js
@@ -0,0 +1,35 @@
+import React from "react"
+
+class StatefulGreetingWithCallback extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            introduction: "Hello",
+            buttonText: "Exit",
+        };
+    }
+
+    handleClick() {
+        this.setState({
+            introduction: "Goodbye!",
+            buttonText: "Enter",
+        }, () => {
+            console.log("New state: ", this.state.introduction);
+            console.log("New state: ", this.state.buttonText);
+        });
+        console.log(this.state.introduction);
+        console.log(this.state.buttonText);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.introduction}, {this.props.name}! {this.props.greeting}</h1>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+            </div>
+        )
+    }
+}
+
+export default StatefulGreetingWithCallback;
