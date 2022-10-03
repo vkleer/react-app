edc3747bdd8fb8ea8c24354ca75b48b4e05b1f8f
Vilayat Kleer <vkleer@hotmail.com>
2022-10-03 00:50:20 +0000

    Create first stateless functional component with props parameter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreeting from './components/FunctionalGreeting';
+import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting />
+      <FunctionalGreetingWithProps greeting="nice to meet you!" name="John" age="30" />
     </div>
   );
 }
---
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const FunctionalGreetingWithProps = (props) => {
+    console.log(props);
+    return <h1>Hello, {props.name}! I see that you're {props.age} years old; {props.greeting}</h1>;
+}
+
+export default FunctionalGreetingWithProps;
