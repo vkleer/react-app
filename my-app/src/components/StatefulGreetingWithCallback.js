ba18cc8b0da091b22321cc84649f169a73e4149b
Vilayat Kleer <vkleer@hotmail.com>
2022-10-03 23:48:40 +0000

    Update handleClick method to use previous state and props for updating values

--- my-app/src/components/StatefulGreetingWithCallback.js
+++ my-app/src/components/StatefulGreetingWithCallback.js
@@ -7,26 +7,38 @@ class StatefulGreetingWithCallback extends React.Component {
         this.state = {
             introduction: "Hello",
             buttonText: "Exit",
+            count: 0,
         };
     }
 
     handleClick() {
-        this.setState({
-            introduction: "Goodbye!",
-            buttonText: "Enter",
-        }, () => {
-            console.log("New state: ", this.state.introduction);
-            console.log("New state: ", this.state.buttonText);
-        });
-        console.log(this.state.introduction);
-        console.log(this.state.buttonText);
+        this.setState((prevState, prevProps) => {
+            console.log("Previous state: ", prevState);
+            console.log("Previous props: ", prevProps);
+            return {
+                introduction: prevState.introduction === "Hello" ? "Goodbye" : "Hello",
+                buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit",
+            }
+        })
+    }
+
+    increment() {
+        this.setState((prevState, prevProps) => {
+            console.log("Previous state: ", prevState);
+            console.log("Previous props: ", prevProps);
+            return {
+                count: prevState.count + 1
+            }
+        })
     }
 
     render() {
         return (
             <div>
-                <h1>{this.state.introduction}, {this.props.name}! {this.props.greeting}</h1>
+                <h1>{this.state.introduction}</h1>
                 <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+                <h2>You've clicked {this.state.count} times</h2>
+                <button onClick={() => this.increment()}>Add 1</button>
             </div>
         )
     }
