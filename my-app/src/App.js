bcc8d65f208219bd6eccbbc7146e7e9935219bf1
Vilayat Kleer <vkleer@hotmail.com>
2022-10-06 22:17:38 +0000

    Install axios, create HTTPRequests component to get jsonplaceholder data and display is in JSON

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,57 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
-import StatefulGreeting from './components/StatefulGreeting';
-import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
-import EventsClass from './components/EventsClass';
-import EventsFunctional from './components/EventsFunctional';
-import EventBinding from './components/EventBinding';
-import ConditionalRenderingClass from './components/ConditionalRenderingClass';
-import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
-import NestingComponents from './components/NestingComponents';
-import MethodsAsPropsParent from './components/MethodsAsPropsParent';
-import RenderingLists from './components/RenderingLists';
-import LifeCyclesCDM from './components/LifeCyclesCDM';
-import LifeCyclesCDU from './components/LifeCyclesCDU';
-import LifeCyclesCWU from './components/LifeCycleCWU';
-import ControlledForm from './components/ControlledForm';
-import UncontrolledForm from './components/UncontrolledForm';
-import SearchBar from './components/SearchBar';
-import HooksCounter from './components/HooksCounter';
-import ControlledFormHooks from './components/ControlledFormHooks';
-import UseStateWithArrays from './components/UseStateWithArrays';
-import UseStateWithObjects from './components/UseStateWithObjects';
-import UseEffectCounter from './components/UseEffectCounter';
-import UseEffectCounterContainer from './components/UseEffectCounterContainer';
+import HTTPRequests from './components/HTTPRequests';
 
 function App() {
-  return (
-    <div className="App">
-      {/* <FunctionalGreetingWithProps greeting="nice to meet you!" name="John" age="30" />
-      <StatefulGreeting greeting="I'm a stateful class component!" name="John" /> */}
-      {/* <StatefulGreetingWithCallback /> */}
-      {/* <EventsClass />
-      <EventsFunctional /> */}
-      {/* <EventBinding /> */}
-      {/* <ConditionalRenderingClass/>
-      <ConditionalRenderingFunctional connected={true}/> */}
-      {/* <NestingComponents /> */}
-      {/* <MethodsAsPropsParent/ > */}
-      {/* <RenderingLists /> */}
-      {/* <LifeCyclesCDM /> */}
-      {/* <LifeCyclesCDU /> */}
-      {/* <LifeCyclesCWU /> */}
-      {/* <ControlledForm /> */}
-      {/* <UncontrolledForm /> */}
-      {/* <SearchBar /> */}
-      {/* <HooksCounter /> */}
-      {/* <ControlledFormHooks /> */}
-      {/* <UseStateWithArrays /> */}
-      {/* <UseStateWithObjects /> */}
-      {/* <UseEffectCounter /> */}
-      <UseEffectCounterContainer />
-    </div>
-  );
+    return (
+      <div className="App">
+        <HTTPRequests />
+      </div>
+    );
 }
 
 export default App;
---
+++ my-app/src/components/HTTPRequests.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export class HTTPRequests extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+        posts: [],
+      }
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            console.log(response);
+            this.setState({
+                posts: response.data
+            });
+        })
+    }
+
+    render() {
+        return (
+        <div>
+            <h1>Posts:</h1>
+            {
+                JSON.stringify(this.state.posts)
+            }
+        </div>
+        )
+  }
+}
+
+export default HTTPRequests
